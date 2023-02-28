5886a9b4fa6f5b62ce074b412dd6b7da9e10a2d9
sabiine-sinicina <124919082+sabiine-sinicina@users.noreply.github.com>
2023-02-28 14:28:54 +0000

    test2

--- tree_height.py
+++ tree_height.py
@@ -40,8 +40,22 @@ def main():
     else:
         return
     print(compute_height(n, parents))
+    
 
+
+    # implement input form keyboard and from files
+    
+    # let user input file name to use, don't allow file names with letter a
+    # account for github input inprecision
+    
+    # input number of elements
+    # input values in one variable, separate with space, split these values in an array
+    # call the function and output it's result
+
+
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-
