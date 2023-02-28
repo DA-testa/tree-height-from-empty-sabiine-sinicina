3ed293bdcd2178903f81ef5e2f03204454bb81cb
sabiine-sinicina <124919082+sabiine-sinicina@users.noreply.github.com>
2023-02-28 15:01:32 +0000

    test1

--- tree_height.py
+++ tree_height.py
@@ -5,41 +5,33 @@ import threading
 
 
 def compute_height(n, parents):
-    # create adjacency list
-    adj_list = [[] for _ in range(n)]
-    for i, p in enumerate(parents):
-        if p != -1:
-            adj_list[p].append(i)
-
-    # perform DFS to compute height
-    def dfs(v):
-        max_height = 0
-        for u in adj_list[v]:
-            max_height = max(max_height, dfs(u))
-        return max_height + 1
-
-    root = parents.index(-1)
-    height = dfs(root)
-    return height
+    # Write this function
+    max_height = 0
+    # Your code here
+    return max_height
 
 
 def main():
-    input_type = input()
-    if input_type == "F":
-        filename = input()
-        if ".a" in filename:
+    input_type= input()
+
+    if "F" in input_type:
+        name = input()
+        if ".a" in name:
             return
-        if "test/" not in filename:
-            filename = "test/" + filename
-        with open(filename) as f:
-            n = int(f.readline().strip())
-            parents = list(map(int, f.readline().strip().split()))
-    elif input_type == "I":
-        n = int(input())
-        parents = list(map(int, input().split()))
-    else:
-        return
-    print(compute_height(n, parents))
+    if "test/" not in name:
+        name = "test/" + name
+    if "test/" name:        
+        with open (name) as f:
+            text = f.readLines()
+            
+
+    elif "I" in input_type:
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == "Success":
+            print("Success")
+        else:
+            print (mismatch)
     
 
 
